8b7b7ffd2d188d9ba85d5a060dd91f9de3d9ad8e
Hector Pena <hpena99@gmail.com>
2018-03-27 18:20:32 -0500

    Submitted for review

--- README.txt
+++
@@ -1,6 +0,0 @@
-README File
-
-HOWTO Running Entertainment Center
-- Copy all file to a directory on your computer
-- Install Python 2.7 if not already installed
-- Double-click on the fresh_tomatoes.html file
--- media.py
+++ media.py
@@ -1,4 +1,6 @@
 import webbrowser
+
+# Class Name: Movie
 class Movie():
     def __init__(self,movie_title, movie_storyline,poster_image,
                  trailer_youtube):
@@ -7,6 +9,5 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         
-
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
